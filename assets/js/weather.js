e07366c1b1237fce1a6fdef527bda8efb8db2e10
momokokong <momokokong@gmail.com>
2024-07-07 22:55:01 -0700

    index.html layout almost done using Tailwind CSS

---
+++ assets/js/weather.js
@@ -0,0 +1,8 @@
+// api key: 1b40ae1a60514ea8a70f1b260e3d46bb
+
+// http://api.openweathermap.org/geo/1.0/direct?q=boston&appid=1b40ae1a60514ea8a70f1b260e3d46bb&limit=1
+
+// api.openweathermap.org/data/2.5/forecast?lat=53.9590555&lon=-1.0815361&appid=1b40ae1a60514ea8a70f1b260e3d46bb&units=imperial
+
+// localstorage:
+// buttons: city name, lat, lon
